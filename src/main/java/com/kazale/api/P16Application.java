f03f3e2a56985e7f2236c45154091edea444c75d
velioterio <velioterio@dxc.com>
2019-09-16 14:56:40 -0300

    Primeiro commit subindo o projeto com JPA Respository

---
+++ src/main/java/com/kazale/api/P16Application.java
@@ -0,0 +1,53 @@
+package com.kazale.api;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.kazale.api.entities.Empresa;
+import com.kazale.api.repositories.EmpresaRepository;
+
+@SpringBootApplication
+public class P16Application {
+	
+	@Autowired
+	private EmpresaRepository empresaRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(P16Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			Empresa empresa = new Empresa();
+			empresa.setRazaoSocial("Kazale IT");
+			empresa.setCnpj("74645215000104");
+			
+			this.empresaRepository.save(empresa);
+
+			List<Empresa> empresas = empresaRepository.findAll();
+			empresas.forEach(System.out::println);			
+			
+			//Optional<Empresa> empresaDb = empresaRepository.findById(1L);
+			Empresa empresaDb = empresaRepository.findById(1L).get();
+			System.out.println("Empresa por ID: " + empresaDb);
+			
+			empresaDb.setRazaoSocial("Kazale IT Web");
+			this.empresaRepository.save(empresaDb);
+				
+			Empresa empresaCnpj = empresaRepository.findByCnpj("74645215000104");
+			System.out.println("Empresa por CNPJ: " + empresaCnpj);
+			
+			this.empresaRepository.deleteById(1L);
+			empresas = empresaRepository.findAll();
+			System.out.println("Empresas: " + empresas.size());
+			
+		};
+	}
+}
